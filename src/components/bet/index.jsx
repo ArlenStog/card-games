bb18d0aa4591ac8ab3f1ce85533a8c77ade60ba9
VojinKrstic <vojinkrstic1@gmail.com>
2022-07-08 16:09:09 +0200

    ikonice za cipove

--- src/components/bet/index.jsx
+++ src/components/bet/index.jsx
@@ -1,26 +1,22 @@
 import React from "react";
+import { ReactComponent as Chip5 } from '../../assets/chip5.svg'
+import { ReactComponent as Chip10 } from '../../assets/chip10.svg'
+import { ReactComponent as Chip15 } from '../../assets/chip15.svg'
+import { ReactComponent as Chip50 } from '../../assets/chip50.svg'
+import { ReactComponent as Chip100 } from '../../assets/chip100.svg'
 
+import "./styles.scss";
 
-
-import './styles.scss';
-
-
-
-const Bet = () =>{
-
-  return(
+const Bet = () => {
+  return (
     <div className="bet">
-        <button className="chip"> 50 </button>
-        
-        <button className="chip"> 100 </button>
-        
-        <button className="chip"> 200 </button>
-
-        <button className="chip"> 500 </button>
-
-        <button className="chip"> 1000 </button>
+      <Chip5 className="chip"/>
+      <Chip10 className="chip"/>
+      <Chip15 className="chip"/>
+      <Chip50 className="chip"/>
+      <Chip100 className="chip"/>
     </div>
-  )
-}
+  );
+};
 
 export default Bet;
--- src/components/table/index.jsx
+++ src/components/table/index.jsx
@@ -1,10 +1,6 @@
 import React from "react";
 
-
-
 import './styles.scss';
-
-
 
 const Table = () =>{
 
